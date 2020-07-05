c9d03ecdbcc548e4f04c460bfbb198ead328a542
Antonio Bernardino da Silva <antoniobsilvaprof@hotmail.com>
2020-07-05 09:30:54 -0300

    Updates

---
+++ precache-manifest.29ee692793c04237907941de12a79405.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9efed17ef7682eb78660bd2bbedd9375",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8bd6c8b7c907865b94d",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c2f2ef187a95013a6b5b",
+    "url": "/static/css/main.4ea2bbd5.chunk.css"
+  },
+  {
+    "revision": "f8bd6c8b7c907865b94d",
+    "url": "/static/js/2.8c120e23.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8c120e23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2f2ef187a95013a6b5b",
+    "url": "/static/js/main.b36862a2.chunk.js"
+  },
+  {
+    "revision": "250103716dfc89e5abb8",
+    "url": "/static/js/runtime-main.9ff9079d.js"
+  },
+  {
+    "revision": "a67f435057bfe83542c80771ba960a35",
+    "url": "/static/media/background.a67f4350.png"
+  },
+  {
+    "revision": "3e936af39b097ee5d75f6597340e1267",
+    "url": "/static/media/loading.3e936af3.gif"
+  }
+]);
