c864137aa826d73cb46c99f237eb0e8359e08304
Antonio Bernardino da Silva <antoniobsilvaprof@hotmail.com>
2020-07-05 09:45:05 -0300

    Updates

---
+++ precache-manifest.c9d0413b3ebdaed85f4f0e9a31f550cd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "655866116ff4e66944e1f73c5a2a65b9",
+    "url": "/Pokedex/index.html"
+  },
+  {
+    "revision": "b5914e70fff73f1d38a8",
+    "url": "/Pokedex/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "eadd4762616c52ad8248",
+    "url": "/Pokedex/static/css/main.4ea2bbd5.chunk.css"
+  },
+  {
+    "revision": "b5914e70fff73f1d38a8",
+    "url": "/Pokedex/static/js/2.df8418af.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Pokedex/static/js/2.df8418af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eadd4762616c52ad8248",
+    "url": "/Pokedex/static/js/main.4a5b059c.chunk.js"
+  },
+  {
+    "revision": "9fb1140b615de1adf3f4",
+    "url": "/Pokedex/static/js/runtime-main.e9efafc6.js"
+  },
+  {
+    "revision": "a67f435057bfe83542c80771ba960a35",
+    "url": "/Pokedex/static/media/background.a67f4350.png"
+  },
+  {
+    "revision": "3e936af39b097ee5d75f6597340e1267",
+    "url": "/Pokedex/static/media/loading.3e936af3.gif"
+  }
+]);
